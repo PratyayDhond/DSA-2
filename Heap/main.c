44b692d5f8e1a6ec0e84146467515dfd3dba7136
Pratyay Dhond <dhondpratyay@gmail.com>
2023-05-18 11:56:21 +0530

    Heap added File input for sort code

--- Heap/main.c
+++ Heap/main.c
@@ -1,25 +1,45 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "heap.h"
 
-int main(){
+int main(int argc, char * argv[]){
     Heap heap;
     initHeap(&heap);
-
+    
     int n;
-    printf("Enter no of elements: ");
-    scanf("%d",&n);
+    if(argc < 2){
+        printf("Enter no of elements: ");
+        scanf("%d",&n);
 
-    int current;
-    int t = n;
-    while(t--){
-        scanf("%d",&current);
-        insert(&heap,current);
+        int current;
+        int t = n;
+        while(t--){
+            scanf("%d",&current);
+            insert(&heap,current);
+        }
+    }else{
+        int temp = 0 ;
+        FILE *fptr = fopen(argv[1],"r+");
+        fscanf(fptr,"%d",&n);        
+        for(int i = 0; i < n; i++){
+            fscanf(fptr,"%d",&temp);
+            insert(&heap,temp);
+        } 
     }
 
+
     heapSort(&heap);
-    // int * ans = heapSortDesc(&heap);
-    // for(int i = 0; i < n; i++)
-        // printf("%d ",ans[i]);
+    printf("Ascending Order : \n");
+    for(int i = 0; i < n; i++)
+        printf("%d ",heap.arr[i]);
+    printf("\n\n");
+
+    // int * arr = heapSortDesc(&heap);
+    // printf("Descending Order : \n");
+    // for(int i = 0 ; i < n; i++)
+        // printf("%d ",arr[i]);
+    // printf("\n");
+    
 
 return 0;
 }
